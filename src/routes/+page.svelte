cd928989bb79bc29064fbf9890f5fda461dd5814
YuxingZhang727 <3072358762@qq.com>
2026-02-01 16:27:09 -0500

    add the button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,23 @@
-<h1>Welcome to my website</h1>
+<script>
+    import Button from './components/button.svelte';
+    const handleClick = (msg) => {
+        alert(msg);
+        console.log(msg);
+    };
+
+</script>
+
+  <Button 
+    label="press me" 
+    bgColor="bg-yellow-500" 
+    onclick={() => handleClick('Hello from Svelte Kit')} 
+  />
+
+
+<h1 class="text-4xl font-extrabold text-transparent bg-clip-text bg-gradient-to-r from-blue-600 to-purple-500 mt-10 mb-6 tracking-tight">
+    Welcome to my website
+</h1>
+
 <p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+
+
---
+++ src/routes/components/button.svelte
@@ -0,0 +1,12 @@
+<script>
+  let { label, bgColor, onclick } = $props();
+</script>
+
+<button 
+  {onclick}
+  class="{bgColor} text-white px-8 py-3 rounded-full font-medium transition-all
+         hover:scale-105 active:scale-95 shadow-lg
+         focus:outline-none focus:ring-4 focus:ring-opacity-50"
+>
+  {label}
+</button>
